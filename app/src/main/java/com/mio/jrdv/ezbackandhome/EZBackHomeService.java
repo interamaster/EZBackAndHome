526d24eadad113b0a5d1874b58f43cbeeae3b2b4
interamaster@gmail.com <Sismed13>
2017-03-30 21:15:16 +0200

    Initial commit

---
+++ app/src/main/java/com/mio/jrdv/ezbackandhome/EZBackHomeService.java
@@ -0,0 +1,155 @@
+package com.mio.jrdv.ezbackandhome;
+
+import android.accessibilityservice.AccessibilityService;
+import android.annotation.SuppressLint;
+import android.graphics.PixelFormat;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.accessibility.AccessibilityEvent;
+import android.widget.ImageView;
+
+public class EZBackHomeService extends AccessibilityService {
+
+    WindowManager windowManager;
+    ImageView back,home,notification,minimize;
+    WindowManager.LayoutParams params;
+    AccessibilityService service;
+
+    @SuppressLint("RtlHardcoded")
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        back = new ImageView(this);
+        home = new ImageView(this);
+        minimize = new ImageView(this);
+        notification = new ImageView(this);
+
+        back.setImageResource(R.drawable.ic_back);
+        home.setImageResource(R.drawable.ic_home);
+        minimize.setImageResource(R.drawable.ic_min);
+        notification.setImageResource(R.drawable.ic_notification);
+
+///////////////////////////////////home//////////////////////////////////////////////
+
+        params= new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.CENTER_VERTICAL|Gravity.RIGHT;
+        params.x = 10;
+        params.y = 50;
+
+        windowManager.addView(home, params);
+
+
+        ///////////////////////////////////minimize//////////////////////////////////////////////
+
+        params= new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.CENTER_VERTICAL|Gravity.RIGHT;
+        params.x = 10;
+        params.y = 250;
+
+        windowManager.addView(minimize, params);
+        ///////////////////////////////////notification//////////////////////////////////////////////
+
+        params= new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.CENTER_VERTICAL|Gravity.RIGHT;
+        params.x = 10;
+        params.y = 450;
+
+        windowManager.addView(notification, params);
+
+        ///////////////////////////////////back//////////////////////////////////////////////
+
+        params= new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.CENTER_VERTICAL|Gravity.RIGHT;
+        params.x = 10;
+        params.y = 650;
+
+        windowManager.addView(back, params);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    performGlobalAction(AccessibilityService.GLOBAL_ACTION_HOME);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    performGlobalAction(AccessibilityService.GLOBAL_ACTION_NOTIFICATIONS);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        minimize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    performGlobalAction(AccessibilityService.GLOBAL_ACTION_RECENTS);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        Log.d("TAG", "onServiceConnected");
+    }
+
+}
---
+++ app/src/main/java/com/mio/jrdv/ezbackandhome/MainActivity.java
@@ -0,0 +1,33 @@
+package com.mio.jrdv.ezbackandhome;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //copiado de:https://androidmads.blogspot.com.es/2016/05/how-create-accessibility-service-in.html
+    //v001 vesion inicial de la wweb con iconos ok
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Permission for Manifest.permission.SYSTEM_ALERT_WINDOW in Android M
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(getApplicationContext())) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, 1);
+            }
+        }
+
+        // Code to start the Service
+        startService(new Intent(getApplication(), EZBackHomeService.class));
+    }
+}
